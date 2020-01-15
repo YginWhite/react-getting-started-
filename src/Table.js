90077ed94df881da8d0495ac657596f72854aa0f
Yrii Sklyarov <yginwhite@gmail.com>
2020-01-15 17:20:40 +0200

    Make two simple components for Table component
    Drop Table component into two simple components.

--- src/Table.js
+++ src/Table.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react';
 
+const TableHeader = () => {
+	return (
+		<thead>
+			<tr>
+				<th>Topic</th>
+				<th>Link</th>
+				<th>Description</th>
+			</tr>
+		</thead>
+	);
+}
+
+const TableBody = () => {
+	return (
+		<tbody>
+			<tr>
+				<td>React</td>
+				<td>https://www.taniarascia.com/getting-started-with-react</td>
+				<td>Getting Started with React - An Overview and Walkthrough Tutorial</td>
+			</tr>
+			<tr>
+				<td>English</td>
+				<td>https://www.native-english.ru/grammar/english-tenses</td>
+				<td>Tenses in English. Table of English tenses.</td>
+			</tr>
+		</tbody>
+	);
+}
+
 class Table extends Component {
 	render() {
 		return (
 			<div>
 			    <table>
-			    	<thead>
-			    		<tr>
-			    			<th>Topic</th>
-			    			<th>Link</th>
-			    			<th>Description</th>
-			    		</tr>
-			    	</thead>
-			    	<tbody>
-			    		<tr>
-			    			<td>React</td>
-			    			<td>https://www.taniarascia.com/getting-started-with-react</td>
-			    			<td>Getting Started with React - An Overview and Walkthrough Tutorial</td>
-			    		</tr>
-			    		<tr>
-			    			<td>English</td>
-			    			<td>https://www.native-english.ru/grammar/english-tenses</td>
-			    			<td>Tenses in English. Table of English tenses.</td>
-			    		</tr>
-			    	</tbody>
+			    	<TableHeader />
+			    	<TableBody />
 			    </table>
 			</div>
 		);

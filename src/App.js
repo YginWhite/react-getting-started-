4a7ddbd343a83d33d02c42894d2bee77c68606de
Yrii Sklyarov <yginwhite@gmail.com>
2020-01-15 20:55:08 +0200

    Create Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
 	state = {
@@ -33,6 +34,7 @@ class App extends Component {
 		return (
 			<div className="container">
 			    <Table linkData={links} removeLink={this.removeLink}/>
+			    <Form />
 			</div>
 		);
 	}
---
+++ src/Form.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+	constructor(props) {
+		super(props);
+
+		this.initialState = {
+			topic: '',
+			link: '',
+			description: ''
+		};
+		this.state = this.initialState;
+	}
+
+	handleChange = (event) => {
+		const { name, value } = event.target;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	render() {
+		const { topic, link, description } = this.state;
+
+		return (
+			<form action="">
+				<label for="topic">Topic</label>
+				<input type="text" name="topic" value={topic} onChange={this.handleChange}/>
+
+				<label for="link">Link</label>
+				<input type="text" name="link" value={link} onChange={this.handleChange}/>
+
+				<label for="description">Description</label>
+				<input type="text" name="description" value={description} onChange={this.handleChange}/>
+			</form>
+		);
+	}
+}
+
+export default Form;
